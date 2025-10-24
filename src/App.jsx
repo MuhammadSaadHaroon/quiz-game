758216afb78f810c6e622bf9f28612e5a4f6a888
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-10-24 18:28:04 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Signup from './pages/Signup';
 import Quiz from './pages/Quiz';
 import Result from './pages/Result';
 import PrivateRoute from './routes/PrivateRoute';
 
-export default function App() {
+export default function App(){
   return (
-    <>
+    <BrowserRouter basename="/quiz-app">
       <Navbar />
       <Routes>
         <Route path="/" element={<Signup />} />
-
-        <Route path="/*" element={
-          <PrivateRoute>
-            <Quiz />
-          </PrivateRoute>
-        } />
-
-        <Route path="result/*" element={
-          <PrivateRoute>
-            <Result />
-          </PrivateRoute>
-        } />
+        <Route path="/quiz" element={<PrivateRoute><Quiz/></PrivateRoute>} />
+        <Route path="/result" element={<PrivateRoute><Result/></PrivateRoute>} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
