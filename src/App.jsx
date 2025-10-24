eae656b7ad39ee9d6f509ed170b175cc01786da2
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-10-24 18:21:46 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,24 @@ import Quiz from './pages/Quiz';
 import Result from './pages/Result';
 import PrivateRoute from './routes/PrivateRoute';
 
-export default function App(){
+export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Signup />} />
-        <Route path="/quiz" element={<PrivateRoute><Quiz/></PrivateRoute>} />
-        <Route path="/result" element={<PrivateRoute><Result/></PrivateRoute>} />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <Quiz />
+          </PrivateRoute>
+        } />
+
+        <Route path="result/*" element={
+          <PrivateRoute>
+            <Result />
+          </PrivateRoute>
+        } />
       </Routes>
     </>
   );
